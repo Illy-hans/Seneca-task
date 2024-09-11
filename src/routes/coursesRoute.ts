41b3a941125500bf947d069b063f7f1a2078d1c3
ilhan <ilhanabdalle@gmail.com>
2024-09-11 13:19:23 +0100

    05 - Adds Router

--- src/routes/coursesRoute.ts
+++ src/routes/coursesRoute.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { statsController } from "../controllers/statsController";
 
 const courseRouter: Router = Router();
 
+courseRouter.post('/:courseId', statsController.createSessionRecordForUser)
+courseRouter.get('/:courseId', statsController.getCourseLifetimeStatsForUser)
 
+courseRouter.get('/:courseId/sessions/:sessionId', statsController.getSessionStatsForUser)
 
 export { courseRouter }; 
